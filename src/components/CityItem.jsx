043d23f25759a2fc7f3149cc7d5a005cc4c9fdba
Yousef <yousefibrahim9@outlook.com>
2025-08-26 21:00:53 +0300

    feat: add country info and enhance city management

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 
 function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id, position } = city;
 
   const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -12,11 +13,13 @@ function CityItem({ city }) {
     }).format(new Date(date));
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,26 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const navigate = useNavigate(); // returns function
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer} onClick={()=>{navigate('form')}}>
+      <h1>Map</h1>
+      <h2>
+        Posistion {lat} - {lng}
+      </h2>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 50 });
+        }}
+      >
+        Change position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
